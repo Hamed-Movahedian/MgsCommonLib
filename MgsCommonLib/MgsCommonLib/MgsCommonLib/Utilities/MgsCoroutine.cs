f4ab4fceb291a78ae1c5a8c462f084d577ff3509
Hamed Movahedian <movahedian.h@gmail.com>
2018-06-25 19:31:53 +0430

    no message

--- MgsCommonLib/MgsCommonLib/MgsCommonLib/Utilities/MgsCoroutine.cs
+++ MgsCommonLib/MgsCommonLib/MgsCommonLib/Utilities/MgsCoroutine.cs
@@ -5,30 +5,40 @@ using UnityEditor;
 
 namespace MgsCommonLib.Utilities
 {
-    public class MgsEditorCoroutine
+    public class MgsCoroutine
     {
         private double _lastTime;
         private Func<bool> _userFunc;
         private double _deltaTime;
 
+        public static string Title;
+        public static string Info;
+        public static float Percentage;
+        private static bool _forceUpdate;
+
+        public static void ForceUpdate()
+        {
+            _forceUpdate = true;
+        }
         public static void Start(IEnumerator enumerator, Func<bool> function, double funcCallDeltaTime)
         {
-            MgsEditorCoroutine editorCoroutine = new MgsEditorCoroutine
+            MgsCoroutine coroutine = new MgsCoroutine
             {
                 _userFunc = function,
                 _deltaTime = funcCallDeltaTime,
                 _lastTime = EditorApplication.timeSinceStartup
             };
-            editorCoroutine.StartEnumerator(enumerator);
+            coroutine.StartEnumerator(enumerator);
         }
         private void StartEnumerator(IEnumerator enumerator)
         {
 
             while (enumerator.MoveNext())
             {
-                if (EditorApplication.timeSinceStartup - _lastTime > _deltaTime)
+                if (EditorApplication.timeSinceStartup - _lastTime > _deltaTime || _forceUpdate)
                 {
                     _lastTime = EditorApplication.timeSinceStartup;
+                    _forceUpdate = false;
                     if (_userFunc())
                         break;
 
